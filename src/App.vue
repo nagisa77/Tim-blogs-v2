84e785cdcb165326bcdf7ea84e91f0618a263e6c
tim <nagisa12164@163.com>
2024-09-28 22:17:58 +0800

    feat: fitst commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,77 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template> 
+  <div class="site">
+    <header class="site-header">
+      <h1 class="header-text header-title">Tim's Blog</h1>
+      <TypingEffect class="header-text header-subtitle" :strings="['在离开之前，一切都是过程. ', '追求自由～ ']"  />
+      <div class="header-text header-route">
+        <li class="header-route-item">
+          <router-link to="/">首页</router-link>
+        </li>
+        <li class="header-route-item">
+          <router-link to="/about">关于</router-link>
+        </li>
+        <li class="header-route-item" >
+          <router-link to="/contact">链接🔗</router-link>
+        </li>
+      </div>
+    </header>
+    <div class="site-content">
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TypingEffect from './components/TypingEffect.vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    TypingEffect,
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.site {
+  display: flex;
+  width: 100%;
+  height: 100%;
 }
+
+.site-header {
+  height: 100vh;
+  width: 300px;
+  padding: 100px 60px;
+  background-color: var(--header-background-color);
+}
+
+.site-content {
+  height: 100vh;
+  width: calc(100% - 300px);
+  background-color: var(--content-background-color);
+}
+
+.header-text {
+  color: var(--header-text-color);
+}
+
+.header-subtitle {
+  padding-top: 30px;
+  font-weight: normal;
+}
+
+.header-route {
+  padding-top: 80px;
+  font-weight: normal;
+  list-style-type: none;
+  line-height: 2;
+}
+
+.header-route-item {
+  transition: font-weight 0.3s ease;
+}
+
+.header-route-item:hover {
+  text-decoration: underline;
+}
+
 </style>
---
+++ src/components/TypingEffect.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <span ref="typed"></span>
+  </div>
+</template>
+
+<script>
+import Typed from 'typed.js';
+
+export default {
+  name: 'TypingEffect',
+  props: {
+    strings: {
+      type: Array,
+      required: true,
+    },
+    typeSpeed: {
+      type: Number,
+      default: 50,
+    },
+    backSpeed: {
+      type: Number,
+      default: 50,
+    },
+    loop: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  mounted() {
+    const options = {
+      strings: this.strings,
+      typeSpeed: this.typeSpeed,
+      backSpeed: this.backSpeed,
+      loop: this.loop,
+    };
+
+    new Typed(this.$refs.typed, options);
+  },
+};
+</script>
